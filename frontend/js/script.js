6f81412cb33e4e2153bf8cd87a40c03f7a1213a7
Diana Galarza <dgalarza@stevens.edu>
2016-10-09 09:25:24 -0400

    Failed to Load Resources problem- to be examined on my Mac computer

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,8 +1,21 @@
-document.getElementById("reportform").onSubmit() = function ( ) {myFunction( ) };
-
-function myFunction(){
-    var formData = $("form.reportform").serializeObject();
+/*window.onload = function myFunction(){
+    var formData = $("form.reportform").serializeArray();
     var xhttp = new XMLHttpRequest();
     xhttp.open("POST", formData, true);
     xhttp.send();
 }
+
+var report = document.getElementById("reportform");
+report.onsubmit = myFunction;    
+*/
+
+$(".submit").submit(function(){
+    var formData = $("form.reportform").serializeArray();
+    $.post("demo_test_post.asp",
+    {
+        formData
+    },
+    function(data, status){
+        alert("Data: " + data + "\nStatus: " + status);
+    });
+});
