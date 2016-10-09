13eff6127466e4a44a4bb281bf1c49af28267899
Ana Parra Vera <anaparravera@gmail.com>
2016-10-08 20:32:45 -0400

    Changed index.html

--- Practice/app.py
+++ Practice/app.py
@@ -1,17 +1,10 @@
 from flask import Flask, render_template
-<<<<<<< HEAD
 
-=======
->>>>>>> 029ca4712f14d792851ed591dec1fd987a3bdbdc
 app = Flask(__name__)
 
 @app.route("/")
 def main():
     return render_template('index.html')
 
-<<<<<<< HEAD
-
-=======
->>>>>>> 029ca4712f14d792851ed591dec1fd987a3bdbdc
 if __name__ == "__main__":
     app.run()
