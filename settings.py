78307b7939a8137199bf7858922e4590d0a6eca4
susana- <susrest3@gmail.com>
2016-10-09 05:53:34 -0400

    Set up REST endpoints

---
+++ settings.py
@@ -0,0 +1,28 @@
+
+MONGO_HOST = 'localhost'
+MONGO_PORT = 27017
+
+MONGO_DBNAME = 'clownsighting'
+
+RESOURCE_METHODS = ['GET', 'POST', 'DELETE']
+ITEM_METHODS = ['GET', 'PATCH', 'PUT', 'DELETE']
+
+schema = {
+    'scarinesslevel' : {'type' : 'string'},
+    'date' : {'type' : 'date'},
+    'address' : { 'type' : 'string' },
+    'city' : {'type' : 'string'},
+    'state' : {'type' : 'string'},
+    'country' : {'type' : 'string'},
+    'continent' : {'type' : 'string'}
+}
+
+sightings = {
+    'additional_lookup' : {
+        'url' : 'regex("[\w]+")',
+        'field' : 'date'
+        },
+    'schema' : schema
+}
+
+DOMAIN = {'sightings': sightings,}
